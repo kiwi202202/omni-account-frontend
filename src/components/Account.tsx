9e839b7a900deab0eb657be105b7c445a7fd8089
kiwi202202 <kiwichen2022@gmail.com>
2024-09-07 23:30:44 +0800

    Update userop history

--- src/components/Account.tsx
+++ src/components/Account.tsx
@@ -18,16 +18,48 @@ import axios from "axios";
 import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "../store";
 import { setAccountDetails } from "../features/account/accountSlice";
+import { ethers } from "ethers";
 
 const Account: React.FC = () => {
   // const [accountDetails, setAccountDetails] = useState<AccountDetails | null>(
   //   null
   // );
-  const { chainId, aaContractAddress, error } = useEthereum();
+  const { chainId, aaContractAddress, error, account } = useEthereum();
   const toast = useToast();
   const accountDetails = useSelector(
     (state: RootState) => state.account.accountDetails
   );
+  const [userOps, setUserOps] = useState<any[]>([]);
+
+  useEffect(() => {
+    const fetchUserOps = async () => {
+      try {
+        const response = await axios.post(
+          process.env.REACT_APP_BACKEND_RPC_URL!,
+          {
+            jsonrpc: "2.0",
+            method: "eth_getUserOpsForAccount",
+            params: [account, aaContractAddress],
+            id: 1,
+          }
+        );
+
+        setUserOps(response.data.result || []);
+      } catch (error) {
+        toast({
+          title: "Failed to fetch transaction history",
+          status: "error",
+          duration: 5000,
+          isClosable: true,
+        });
+        console.error("Failed to fetch transaction history:", error);
+      }
+    };
+
+    if (aaContractAddress) {
+      fetchUserOps();
+    }
+  }, [account, aaContractAddress, chainId]);
 
   if (!accountDetails) {
     return <Text>Loading...</Text>;
@@ -57,23 +89,39 @@ const Account: React.FC = () => {
           <Table>
             <Thead>
               <Tr>
-                <Th>Tx Hash</Th>
-                <Th>From</Th>
-                <Th>To</Th>
-                <Th>Value</Th>
-                <Th>Timestamp</Th>
+                {/* <Th>Sender</Th> */}
+                <Th>Chain Id</Th>
+                <Th>Nonce</Th>
+                <Th>Call Data</Th>
+                <Th>Total Gas Cost</Th>
               </Tr>
             </Thead>
+
             <Tbody>
-              {accountDetails.history.map((tx, index) => (
-                <Tr key={index}>
-                  <Td>{tx.hash}</Td>
-                  <Td>{tx.from}</Td>
-                  <Td>{tx.to}</Td>
-                  <Td>{tx.value} ETH</Td>
-                  <Td>{tx.timestamp}</Td>
-                </Tr>
-              ))}
+              {userOps.map((tx, index) => {
+                const totalGasLimit =
+                  parseInt(tx.callGasLimit, 16) +
+                  parseInt(tx.verificationGasLimit, 16) +
+                  parseInt(tx.preVerificationGasLimit, 16);
+
+                const totalGasCost =
+                  totalGasLimit *
+                  (parseInt(tx.maxFeePerGas, 16) +
+                    parseInt(tx.maxPriorityFeePerGas, 16));
+
+                const totalGasCostInEth = ethers.formatEther(
+                  totalGasCost.toString()
+                );
+
+                return (
+                  <Tr key={index}>
+                    <Td>{parseInt(tx.chainId, 16)}</Td>
+                    <Td>{parseInt(tx.nonce, 16)}</Td>
+                    <Td>{tx.callData}</Td>
+                    <Td>{totalGasCostInEth} ETH</Td>
+                  </Tr>
+                );
+              })}
             </Tbody>
           </Table>
         </Box>
